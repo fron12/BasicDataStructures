fe4a94417feadfdf325cf1e344cc0bf727441fcb
Fred <sohnf90@yahoo.com>
2019-03-07 01:11:34 -0800

    Do splice

---
+++ Splice.js
@@ -0,0 +1,7 @@
+let myArray = ["number", "nice", 4, "hamburger"];
+
+myArray.splice(1, 1);
+
+console.log(myArray);
+
+// splice(initial index, how many to remove, add to index starting at initial index(optional))
