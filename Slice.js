b7b7c739f5dd5cd2927259953dc995adba9065a7
Fred <sohnf90@yahoo.com>
2019-03-07 11:01:21 -0800

    Practiced slice

---
+++ Slice.js
@@ -0,0 +1,6 @@
+let myArray = ["red", "revenge", "rad", "relocate"];
+
+// slice(index to begin at, index to stop extraction(up to but not including));
+
+let mySlice = myArray.slice(0,2);
+console.log(mySlice);
