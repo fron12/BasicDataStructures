d7383fdaa79d71d473d62bd9dd1e05a351a12648
Fred <sohnf90@yahoo.com>
2019-03-07 01:21:46 -0800

    Added using splice method

--- Splice.js
+++ Splice.js
@@ -2,6 +2,10 @@ let myArray = ["number", "nice", 4, "hamburger"];
 
 myArray.splice(1, 1);
 
-console.log(myArray);
+// console.log(myArray);
 
 // splice(initial index, how many to remove, add to index starting at initial index(optional))
+
+myArray.splice(3, 0, "please", "thank you.");
+
+console.log(myArray);
