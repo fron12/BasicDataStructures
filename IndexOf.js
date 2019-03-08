102c11f2896c38a5fe7bb95c4c29b0e8bc3625a8
Fred <sohnf90@yahoo.com>
2019-03-08 00:05:47 -0800

    Practiced IndexOf

---
+++ IndexOf.js
@@ -0,0 +1,4 @@
+let myArray = ["Thunder", "Talon", "Tiger", "Thistle"];
+
+console.log(myArray.indexOf("Thunder")); // checks array for element given("Thunder")
+console.log(myArray.indexOf("Thailand"));
