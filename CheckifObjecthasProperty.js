0b3a715846655e6415b6f745670efd8e3d129f14
Fred <sohnf90@yahoo.com>
2019-03-12 01:09:13 -0700

    Practiced checking for object properties

---
+++ CheckifObjecthasProperty.js
@@ -0,0 +1,9 @@
+let sWords = {
+    sand: "everywhere",
+    salt: "gamer mines",
+    sun: "vitamin D",
+    star: "north"
+}
+
+console.log(sWords.hasOwnProperty("sand"));
+// console.log("salt" in sWords);
